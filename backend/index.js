d0f0360abb649eb8b932e5a9ba8125864baffb33
DevGamer9991 <pathornton114@gmail.com>
2022-11-18 21:44:15 -0600

    Got Simple Screen and Server communication setup

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 const express = require("express")
-const { createServer, Server } = require("http")
-const { server } = require("socket.io");
+const { createServer } = require("http")
+const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
 
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on("connection", (socket) => {
-
+    io.emit("test", "Hi")
 })
 
 app.get("/api", function (req, res) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import socketIOClient from "socket.io-client"
+import styled from "styled-components"
+
+const ENDPOINT = "http://localhost:8080"
+
+const Main = styled.div`
+  background-color: var(--main-color);
+  height: 100vh;
+`;
+
+const NavWrapper = styled.div`
+  background: var(--main-color);
+  color: white;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding-inline: 10px;
+  position: relative;
+`;
+
+const NavTitle = styled.h1`
+    text-align: center;
+    width: 100%;
+`;
+
+const MainBox = styled.div`
+  width: 300px;
+  height: 250px;
+  background-color: white;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  left: 50%;
+  border-radius: 5px;
+  padding: 5px;
+`;
+
+const MainBoxTitle = styled.h2`
+  width: 100%;
+  text-align: center;
+`;
 
 function App() {
+  
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("test", data => {
+      setResponse(data)
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <NavWrapper>
+        <NavTitle>Aux Website</NavTitle>
+      </NavWrapper>
+      <MainBox>
+        <MainBoxTitle>Create Session</MainBoxTitle>
+        <p>{response}</p>
+      </MainBox>
+    </Main>
   );
 }
 
